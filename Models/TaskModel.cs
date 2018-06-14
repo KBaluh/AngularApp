4f178cbae602a7c9a6b225de5337acef4d5a2819
Balukh Kostya <baluh.kostya@gmail.com>
2018-06-15 01:14:07 +0300

    Append Model project with TaskModel

---
+++ Models/TaskModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public class TaskModel
+    {
+        public int TaskModelId { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
