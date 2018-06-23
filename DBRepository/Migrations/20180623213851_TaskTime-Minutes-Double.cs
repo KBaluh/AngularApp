52355d67a87f7a0a9e7945062588e77d4b9325b0
Balukh Kostya <baluh.kostya@gmail.com>
2018-06-24 00:55:18 +0300

    Change TaskTime field type from int to double

---
+++ DBRepository/Migrations/20180623213851_TaskTime-Minutes-Double.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DBRepository.Migrations
+{
+    public partial class TaskTimeMinutesDouble : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Minutes",
+                table: "TaskTimes",
+                nullable: false,
+                oldClrType: typeof(int));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Minutes",
+                table: "TaskTimes",
+                nullable: false,
+                oldClrType: typeof(double));
+        }
+    }
+}
