f2a92cb0e0af233fce6609ecc0911d7c8b8e3b12
Balukh Kostya <baluh.kostya@gmail.com>
2018-06-23 18:18:09 +0300

    Pass task id from task-card to task-time component

--- AngularApp/ClientApp/src/app/tasks/task-time/task-time.component.ts
+++ AngularApp/ClientApp/src/app/tasks/task-time/task-time.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-task-time',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task-time.component.css']
 })
 export class TaskTimeComponent implements OnInit {
+
+  @Input() taskModelId: number;
 
   constructor() { }
 
